463c782830e2d04bb515d95ad4b8f336b30146cc
LStAa <69948449+lasata20@users.noreply.github.com>
2024-02-28 14:55:04 +0545

    Initial commit

---
+++ BasicJS/starter.js
@@ -0,0 +1,40 @@
+// let massmark = 78;
+// let heightmark = 1.69;
+// let massjohn = 92;
+// let heightjohn = 1.95;
+
+// let BMIMark = massmark / (heightmark*heightmark);
+// let BMIJohn = massjohn / (heightjohn*heightjohn);
+
+// console.log(BMIMark);
+// console.log(BMIJohn);
+
+// // markHigherBMI = BMIMark>BMIJohn;
+// // console.log(markHigherBMI);
+
+// if (BMIMark>BMIJohn){
+//     console.log(`Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})`);
+// }else{
+//     console.log(`John's BMI (${BMIJohn}) is higher than Mark's BMI (${BMIMark})`)
+// }
+
+
+
+// const scoreDolphins = (96+108+89) / 3;
+// const scoreKoala = (88+91+110) / 3;
+
+// console.log(scoreDolphins);
+// console.log(scoreKoala);
+
+// if (scoreDolphins>scoreKoala){
+//     console.log("Dolphins win the trophy.");
+// }else if (scoreDolphins<scoreKoala){
+//     console.log("Koalas win the trophy.");
+// }else{
+//     console.log("Both win the trophy.")
+// }
+
+
+// const bill = 430;
+// const tip = bill>=50 && bill<=300 ? bill*0.15 : bill*0.2;
+// console.log(`The bill was ${bill}, tip was ${tip} and total is ${bill + tip}`);
